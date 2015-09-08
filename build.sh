eb6e25e4cfad5191a759e0562994a00dfd374c7f
Sriram Panyam <sri.panyam@gmail.com>
2015-09-08 01:29:56 -0700

    Adding manifest to buidl

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ javac `find . | grep .java`
 
 # echo "Creating jar..."
 cd src 
-jar -cvfm snake.jar manifest `find ./ | grep .class`
+jar -cvfm snake.jar ../manifest `find ./ | grep .class`
 mv snake.jar ../
